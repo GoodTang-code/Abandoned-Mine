f119b908bb5a6945608190d0e63f87b73277bc31
GoodTang-code <70703655+GoodTang-code@users.noreply.github.com>
2020-09-19 20:27:54 +0700

    Camera Follow Size

--- Abandoned Mine/Assets/Code/Camera.cs
+++ Abandoned Mine/Assets/Code/Camera.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D player;
+    public float smooth = 0.125f; // 0 to 1
+    public Vector3 offset;
 
-    // Update is called once per frame
-    void Update()
+    public float desiredCamSize;
+    public float velo;
+    public float maxThruster = 10f;
+    public float[] camSize = new float[3];
+
+    void FixedUpdate()
     {
-        transform.rotation = Quaternion.Euler(Vector3.zero);
+        Vector3 desiredPos = new Vector3(player.position.x, player.position.y, -5f) + offset;
+        //Vector3 desiredPos = player.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smooth);
+        transform.position = smoothedPos;
+
+        //transform.LookAt(player);
+
+        velo = Mathf.Pow(player.velocity.x, 2) + Mathf.Pow(player.velocity.y,2);
+        desiredCamSize = ((velo / maxThruster) * (camSize[2] - camSize[1])) + camSize[1];
+        float smoothCamSize = Mathf.Lerp(UnityEngine.Camera.main.orthographicSize, desiredCamSize, smooth); 
+
+
+
+        UnityEngine.Camera.main.orthographicSize = smoothCamSize;
     }
 }
