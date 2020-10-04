8794f6e6339e51e52ebfb92b2c907c5766fa5531
GoodTang-code <70703655+GoodTang-code@users.noreply.github.com>
2020-10-04 13:07:01 +0700

    Parallex Scrolling

---
+++ Abandoned Mine/Assets/SO/Code/FloatVariableSingle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/FloatVariableSingle")]
+public class FloatVariableSingle : ScriptableObject
+{
+    public float Value;
+}
+
+[System.Serializable]
+public class FloatReference
+{
+    public bool useConstant = true;
+    public float constantValue;
+    public FloatVariableSingle Variable;
+
+    public float Value
+    {
+        get {
+            return useConstant ? constantValue : Variable.Value;
+        }
+
+    }
+
+}
